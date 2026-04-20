b4acb7a348bfe4f5f2891b65a51fb1291ad3e4cf
Paulo Henrique <pauloramosh26@gmail.com>
2026-04-20 19:45:43 -0300

    Primeiro exercício do curso
    Estou aprendendo os fundamentos do Console.log().
    No meu exercício eu tinha que imprimir um valor com "String" e number misturado no mesmo parâmetros.

---
+++ CursoJS-Udemy/aula1/Exercicios/Exercicio01.js
@@ -0,0 +1,9 @@
+console.log( 'Meu nome é paulo. E eu estou aprendendo JavaScript as ', 10 , ' da manhã')
+
+
+//Outra forma mais avançada
+
+let nome = "Paulo"
+let horario = 10
+
+console.log('Meu nome é ' + nome +'. '+ 'E eu estou aprendendo JavaScript as ' + horario + ' da manhã')
