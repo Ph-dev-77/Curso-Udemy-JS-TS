63e266b5ccadf29bd57c77a43fbe348c40015298
Paulo Henrique <pauloramosh26@gmail.com>
2026-05-07 20:56:54 -0300

    configurando as pastas

---
+++ CursoJS-Udemy/aula04/index.js
@@ -0,0 +1,5 @@
+//Objeto MATH
+let num1 = 9.5677;
+//let num2 = Math.floor(num1)
+let num2 = Math.ceil(num1)
+console.log(num2)
--- CursoJS-Udemy/aula1/app.js
+++
@@ -1,3 +0,0 @@
-var nome = "Paulo";
-
-console.log("Meu nome é " + nome + ". Estou aprendendo JovaScript as 10 da manhã");
